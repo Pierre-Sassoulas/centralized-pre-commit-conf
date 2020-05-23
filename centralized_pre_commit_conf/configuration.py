2868108caefdd0325baf45245eeb69b9db052f66
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-05-23 08:30:25 +0200

    Add a configuration constant for dependencies

--- centralized_pre_commit_conf/configuration.py
+++ centralized_pre_commit_conf/configuration.py
@@ -2,3 +2,4 @@ CONFIG_FILES = [".flake8", ".isort.cfg", ".pre-commit-config.yaml", ".pylintrc"]
 DEFAULT_REPOSITORY = "https://raw.githubusercontent.com/Pierre-Sassoulas/centralized-pre-commit-conf"
 DEFAULT_BRANCH = "master"
 DEFAULT_PATH = "centralized_pre_commit_conf/static"
+DEPENDENCIES = ["black==19.3b0", "isort==4.3.15", "pylint", "flake8"]
