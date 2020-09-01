f85df6ae04e49b1a7c45f95c09c3b9bfcc322d10
Pierre Sassoulas <pierre.sassoulas@edf-sf.com>
2020-09-01 15:46:23 +0200

    Add an integration test for main function

---
+++ tests/test_integration.py
@@ -0,0 +1,39 @@
+import sys
+import unittest
+from typing import List
+
+from centralized_pre_commit_conf.main import run
+
+URL = "https://raw.githubusercontent.com/Pierre-Sassoulas/centralized-pre-commit-conf"
+
+
+class TestIntegration(unittest.TestCase):
+    def test_help(self):
+        self.assert_exit_with_code(["pre-commit-conf", "-h"], 0)
+
+    def test_bad_arg(self):
+        self.assert_exit_with_code(["pre-commit-conf", "--not-exist"], 2)
+
+    def test_normal_args(self):
+        self.assert_exit_with_code(
+            [
+                "pre-commit-conf",
+                "--repository",
+                URL,
+                "--branch",
+                "master",
+                "--path",
+                "centralized_pre_commit_conf/static",
+                "-f",
+                "--insecure",
+                "-vv",
+            ],
+            0,
+        )
+
+    def assert_exit_with_code(self, command: List[str], expected_code: int):
+        sys.argv = command
+        with self.assertRaises(SystemExit) as sys_exit:
+            run()
+        error_msg = f"{sys_exit} exited with the wrong error code ({sys_exit.exception.code}) for args: {command}"
+        assert sys_exit.exception.code == expected_code, error_msg
