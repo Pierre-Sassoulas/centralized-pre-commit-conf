d98c3c1aa3edf94d02c41cf4965d6129c45fc9d9
Pierre Sassoulas <pierre.sassoulas@edf-sf.com>
2020-09-02 17:58:31 +0200

    Fix the compatibility problem with python 3.6

--- centralized_pre_commit_conf/install.py
+++ centralized_pre_commit_conf/install.py
@@ -16,6 +16,13 @@ def install(url, config_files, replace_existing=False, verbose=False, insecure=F
     update_gitignore(config_files, verbose)
 
 
+def subprocess_compat_mode(commands):
+    if sys.version_info.minor >= 7:
+        return subprocess.run(commands, capture_output=True)
+    # python < 3.7
+    return subprocess.run(commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
 def install_pre_commit(verbose):
     if not Path(".pre-commit-config.yaml").exists():
         warn("No '.pre-commit-config.yaml' found, we can't install pre-commit.")
@@ -23,9 +30,9 @@ def install_pre_commit(verbose):
     install_pre_commit_command = ["pip3", "install", "pre-commit==1.14.0"]
     if verbose:
         info(f"Launching : {install_pre_commit_command}")
-    subprocess.run(install_pre_commit_command, capture_output=True)
+    subprocess_compat_mode(install_pre_commit_command)
     init_pre_commit = ["pre-commit", "install"]
     if verbose:
         info(f"Launching : {init_pre_commit}")
-    subprocess.run(init_pre_commit, capture_output=True)
+    subprocess_compat_mode(init_pre_commit)
     success("🎉 pre-commit installed locally with the current configuration. 🎉")
