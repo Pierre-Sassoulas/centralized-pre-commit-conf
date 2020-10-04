083dc3d2a28e9d9aba0ce3081a9a8bae2052a5a5
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-10-04 16:26:29 +0200

    Less arguments passing in the code

--- centralized_pre_commit_conf/main.py
+++ centralized_pre_commit_conf/main.py
@@ -5,7 +5,7 @@ import confuse
 
 from centralized_pre_commit_conf.constants import APPLICATION_NAME, ExitCode
 from centralized_pre_commit_conf.install import install
-from centralized_pre_commit_conf.parse_args import get_url_from_args, parse_args
+from centralized_pre_commit_conf.parse_args import parse_args
 from centralized_pre_commit_conf.prints import error, info
 
 
@@ -16,26 +16,12 @@ def run():
     except confuse.ConfigError as e:
         error(f"Problem with your configuration file in {[s.filename for s in config.sources]}: {e}")
         sys.exit(ExitCode.PRE_COMMIT_CONF_NOT_FOUND.value)
-    url = get_url_from_args(config["repository"].get(str), config["branch"].get(str), config["path"].get(str))
-    config_files = config["configuration_files"].get(list)
-    verbose = config["verbose"].get(bool)
-    replace_existing = config["replace_existing"].get(bool)
-    insecure = config["insecure"].get(bool)
-    update_gitignore_on_install = config["update_gitignore"].get(bool)
     config_path = Path(config.config_dir()).resolve() / "config.yaml"
     if not config_path.exists():
         info(f"You can set the option system wide in {config_path}.")
-    if verbose:
+    if config["verbose"].get(bool):
         info(f"Installing with the following options : {config}.")
-        info(f"Configuration files to fetch : {config_files}.")
-    install(
-        url=url,
-        config_files=config_files,
-        replace_existing=replace_existing,
-        verbose=verbose,
-        insecure=insecure,
-        update_gitignore_on_install=update_gitignore_on_install,
-    )
+    install(config)
     sys.exit(ExitCode.OK.value)
 
 
