8a7a71ae3d5b6309becb580e4db98da4ff5da324
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-06-29 08:41:13 +0200

    Add pydocstringformatter to the pre-commit configuration

--- centralized_pre_commit_conf/__init__.py
+++ centralized_pre_commit_conf/__init__.py
@@ -1,6 +1,6 @@
 """
 Easily install and update centralized pre-commit hooks and their configuration files
-in decentralized repositories
+in decentralized repositories.
 """
 
 from centralized_pre_commit_conf.install import install
