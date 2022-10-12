ccd812a91320c45bda40852addaf8ab3e59a4958
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-10-12 17:37:16 +0200

    Permit to print emojis on Windows
    Equivalent to print need a newline

--- centralized_pre_commit_conf/prints.py
+++ centralized_pre_commit_conf/prints.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Union
 
 from colorama import Fore, init
@@ -6,7 +7,12 @@ init()
 
 
 def echo(color: Fore, message: Union[Warning, str]) -> None:
-    print(f"{color}{message}{Fore.RESET}")
+    """Also permits to print emojis on Windows.
+
+    To support non-UTF-8 environments like Windows, we need
+    to explicitly encode the message instead of using plain print()
+    """
+    sys.stdout.buffer.write(f"{color}{message}{Fore.RESET}\n".encode())
 
 
 def info(message: str) -> None:
